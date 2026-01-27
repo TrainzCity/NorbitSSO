a81776c577cf8e7501e780b6414ebefca5fd355b
Глеб Гребенщиков <Glebka_1234@trainzcity.myftp.org>
2026-01-27 21:40:42 +0300

    Добавлена субмодель

---
+++ src/NoribtSSO/AuthWEB/Model/User.cs
@@ -0,0 +1,25 @@
+﻿namespace AuthWEB.Model
+{
+    public partial class User
+    {
+        public Guid Uuid { get; set; }
+
+        public string Surname { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Patronymic { get; set; }
+
+        public DateOnly Birthday { get; set; }
+
+        public string? Email { get; set; }
+
+        public string Phone { get; set; } = null!;
+
+        public string Login { get; set; } = null!;
+
+        public byte[] Password { get; set; } = null!;
+
+        public bool IsBlocked { get; set; }
+    }
+}
