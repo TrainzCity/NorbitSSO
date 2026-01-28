549f2a61a885f41732701da0f1735c0fb9af295a
Глеб Гребенщиков <gleb@trainzcity.com>
2026-01-28 18:21:55 +0300

    Фикс циклической обработки ответов БД

--- src/NoribtSSO/NorbitApi/Program.cs
+++ src/NoribtSSO/NorbitApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using NorbitApi.Model;
 namespace NorbitApi
 {
@@ -9,7 +10,10 @@ namespace NorbitApi
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
